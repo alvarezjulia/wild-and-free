f3c545c9994e0fe5de8e1856045ef2bfd1cee881
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-04-02 10:11:08 +0200

    Final seutp w3d1-testing turning flashlight

--- scripts/savior.js
+++ scripts/savior.js
@@ -2,21 +2,24 @@ class Savior {
     constructor() {
         this.saviorX = 50
         this.saviorY = 40
+        this.bleftX = this.saviorX + 40 - 68
+        this.bleftY = this.saviorY + 85 - 30
+        this.brightX = this.saviorX + 96 - 68
+        this.brightY = this.saviorY + 85 - 30
+        this.angle = 0
     }
 
     draw() {
         fill('rgba(240,243,46, 0.80)')
         noStroke()
+        push()
+        translate(this.saviorX, this.saviorY)
+        angleMode(DEGREES)
+        rotate(this.angle)
         // rect(this.saviorX, this.saviorY, 55, 55, 0, 40, 65, 50)
-        triangle(
-            this.saviorX + 40,
-            this.saviorY + 85,
-            this.saviorX + 68,
-            this.saviorY + 30,
-            this.saviorX + 96,
-            this.saviorY + 85
-        )
-        arc(this.saviorX + 68, this.saviorY + 85, 56, 56, 0, PI)
+        triangle(this.bleftX, this.bleftY, this.saviorX, this.saviorY, this.brightX, this.brightY)
+        // arc(this.saviorX, this.saviorY, 56, 56, 0, PI)
+        pop()
 
         // beginShape(TRIANGLES)
         // vertex(this.saviorX + 60, this.saviorY + 20)
@@ -37,19 +40,24 @@ class Savior {
 
         //Savior moves with arrow keys
         if (keyIsDown(LEFT_ARROW)) {
-            this.saviorX -= MOVE
+            translate(this.saviorX, this.saviorY)
+            this.angle += 5
         }
 
         if (keyIsDown(RIGHT_ARROW)) {
-            this.saviorX += MOVE
+            this.angle -= 5
         }
 
         if (keyIsDown(UP_ARROW)) {
             this.saviorY -= MOVE
+            this.bleftY -= MOVE
+            this.brightY -= MOVE
         }
 
         if (keyIsDown(DOWN_ARROW)) {
             this.saviorY += MOVE
+            this.bleftY += MOVE
+            this.brightY += MOVE
         }
     }
 }
